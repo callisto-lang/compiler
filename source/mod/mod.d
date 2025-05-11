1f583b8473fcbe916bbe9302693faa11a7669098
MESYETI <mesyeti@protonmail.com>
2025-05-11 16:18:10 +0100

    module section definitions done

---
+++ source/mod/mod.d
@@ -0,0 +1,13 @@
+module callisto.mod.mod;
+
+import std.stdio;
+import callisto.mod.sections;
+
+class Module {
+	HeaderSection header;
+	Section[]     sections;
+
+	this() {
+		
+	}
+}
--- source/mod/sections.d
+++ source/mod/sections.d
@@ -4,6 +4,30 @@ import std.conv;
 import std.stdio;
 import std.bitmanip;
 
+alias SectionInt = ulong;
+
+enum ModCPU : SectionInt {
+	None    = 0x0000,
+	x86_64  = 0x0001,
+	RM86    = 0x0002,
+	ARM64   = 0x0003,
+	Uxn     = 0x0004,
+	Fox32   = 0x0005,
+	M68k    = 0x0006,
+	RISCV   = 0x0007
+}
+
+enum ModOS : SectionInt {
+	None            = 0x0000,
+	Linux           = 0x0001,
+	macOS           = 0x0002,
+	Windows         = 0x0003,
+	DOS             = 0x0004,
+	FreeBSD         = 0x0005,
+	Varvara         = 0x0006,
+	Fox32OS         = 0x0007
+}
+
 enum SectionType : ubyte {
 	TopLevel  = 0x00,
 	FuncDef   = 0x01,
@@ -15,12 +39,9 @@ enum SectionType : ubyte {
 	Union     = 0x07,
 	Alias     = 0x08,
 	Implement = 0x09,
-	Let       = 0x0A
-
-	// TODO: structures
+	Let       = 0x0A,
+	Struct    = 0x0B
 }
-
-alias SectionInt = ulong;
 
 class SectionException : Exception {
 	this(string msg, string file = __FILE__, size_t line = __LINE__) {
@@ -110,6 +131,33 @@ private void WriteInt(File file, SectionInt value) {
 
 private void WriteByte(File file, ubyte value) {
 	file.rawWrite([value]);
+}
+
+
+class HeaderSection : Section {
+	SectionInt ver;
+	ModCPU     cpu;
+	ModOS      os;
+	SectionInt sectionNum;
+	string     source;
+
+	override SectionType GetType() => assert(0);
+
+	override void Write(File file) {
+		file.WriteInt(ver);
+		file.WriteInt(cast(SectionInt) cpu);
+		file.WriteInt(cast(SectionInt) os);
+		file.WriteInt(sectionNum);
+		file.WriteString(source);
+	}
+
+	override void Read(File file, bool skip) {
+		ver        = file.ReadInt();
+		cpu        = cast(ModCPU) file.ReadInt();
+		os         = cast(ModOS)  file.ReadInt();
+		sectionNum = file.ReadInt();
+		source     = file.ReadString();
+	}
 }
 
 class TopLevelSection : Section {
@@ -225,5 +273,141 @@ class EnumSection : Section {
 }
 
 class RestrictSection : Section {
-	
+	string ver;
+
+	override SectionType GetType() => SectionType.Restrict;
+	override void        Write(File file) => file.WriteString(ver);
+	override void        Read(File file, bool skip) => cast(void) (ver = file.ReadString());	
+}
+
+class UnionSection : Section {
+	SectionInt size;
+	string     name;
+
+	override SectionType GetType() => SectionType.Union;
+
+	override void Write(File file) {
+		file.WriteInt(size);
+		file.WriteString(name);
+	}
+
+	override void Read(File file, bool skip) {
+		size = file.ReadInt();
+		name = file.ReadString();
+	}
+}
+
+class AliasSection : Section {
+	string original;
+	string newName;
+
+	override SectionType GetType() => SectionType.Alias;
+
+	override void Write(File file) {
+		file.WriteString(original);
+		file.WriteString(newName);
+	}
+
+	override void Read(File file, bool skip) {
+		original = file.ReadString();
+		newName  = file.ReadString();
+	}
+}
+
+class ImplementSection : Section {
+	string   type;
+	string   method;
+	string[] called;
+	string   assembly;
+
+	override SectionType GetType() => SectionType.Implement;
+
+	override void Write(File file) {
+		file.WriteString(type);
+		file.WriteString(method);
+		file.WriteStringArray(called);
+		file.WriteString(assembly);
+	}
+
+	override void Read(File file, bool skip) {
+		type   = file.ReadString();
+		method = file.ReadString();
+		called = file.ReadStringArray();
+
+		if (!skip) {
+			assembly = file.ReadString();
+		}
+	}
+}
+
+class LetSection : Section {
+	bool       array;
+	SectionInt size;
+	string     type;
+	string     name;
+
+	override SectionType GetType() => SectionType.Let;
+
+	override void Write(File file) {
+		file.WriteInt(array? 1 : 0);
+		file.WriteInt(size);
+		file.WriteString(type);
+		file.WriteString(name);
+	}
+
+	override void Read(File file, bool skip) {
+		array = file.ReadInt() != 0;
+		size  = file.ReadInt();
+		type  = file.ReadString();
+		name  = file.ReadString();
+	}
+}
+
+struct ModStructEntry {
+	bool       ptr;
+	string     type;
+	bool       array;
+	SectionInt size;
+	SectionInt offset;
+	string     name;
+}
+
+class StructSection : Section {
+	SectionInt       size;
+	string           name;
+	ModStructEntry[] entries;
+
+	override SectionType GetType() => SectionType.Struct;
+
+	private static void WriteEntry(File file, ref ModStructEntry entry) {
+		file.WriteInt(entry.ptr? 1 : 0);
+		file.WriteString(entry.type);
+		file.WriteInt(entry.array? 1 : 0);
+		file.WriteInt(entry.size);
+		file.WriteInt(entry.offset);
+		file.WriteString(entry.name);
+	}
+
+	private static ModStructEntry ReadEntry(File file) {
+		ModStructEntry ret;
+
+		ret.ptr    = file.ReadInt() != 0;
+		ret.type   = file.ReadString();
+		ret.array  = file.ReadInt() != 0;
+		ret.size   = file.ReadInt();
+		ret.offset = file.ReadInt();
+		ret.name   = file.ReadString();
+
+		return ret;
+	}
+
+	override void Write(File file) {
+		file.WriteInt(size);
+		file.WriteString(name);
+		file.WriteInt(cast(SectionInt) entries.length);
+
+		foreach (ref entry ; entries) {
+			WriteEntry(file, entry);
+		}
+	}
 }
